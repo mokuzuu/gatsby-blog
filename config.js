783daa514a34d6c52496d88903c96246c79e8da7
Taiga <mokuzuzzz@gmail.com>
2020-08-21 00:17:24 +1000

    change details

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'Mokuzu', // Required
+  author: 'Mokuzu', // Required
+  description: 'Mokuzu blog site',
+  primaryColor: '#5c61ff', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://mokuzu.tech',
+    github: 'https://github.com/mokuzuu',
+    twitter: 'https://twitter.com/mokuzu_1998',
+    linkedin: 'https://www.linkedin.com/in/taiga-matsumoto/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://mokuzu.tech',
 };
